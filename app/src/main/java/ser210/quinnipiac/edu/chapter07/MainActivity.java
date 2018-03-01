a4120ca0fc29736c3f355a7e169ce41d3b8141fd
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2018-02-28 22:44:51 -0500

    Initial commit

---
+++ app/src/main/java/ser210/quinnipiac/edu/chapter07/MainActivity.java
@@ -0,0 +1,33 @@
+package ser210.quinnipiac.edu.chapter07;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void itemClicked(long id){
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            details.setWorkoutId(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
+            Intent intent = new Intent (this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID,(int)id);
+            startActivity(intent);
+        }
+    }
+}
